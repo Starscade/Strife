45a03c1466a6eb4f5f0651c9d3b0c05fd825fd47
Starscade <angus@starsca.de>
2026-09-04 16:39:52 -0700

    Broke up long build script line.

--- install.sh
+++ install.sh
@@ -5,4 +5,9 @@ INSTALL_DIR=~/.local/bin
 mkdir -p "$INSTALL_DIR" &&
 go mod tidy &&
 go fmt . &&
-CGO_ENABLED=0 go build -ldflags="-s -w" -v -x -o "${INSTALL_DIR}/Strife" .
+CGO_ENABLED=0 &&
+go build \
+	-ldflags="-s -w" \
+	-v \
+	-x \
+	-o "${INSTALL_DIR}/Strife" .
